d5387fbe15361b4379faf1eca83f01dee0192fd4
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2020-01-22 00:02:50 +0900

    Delete create-npmrc.sh

--- npm/create-npmrc.sh
+++
@@ -1,4 +0,0 @@
-npm config set strict-ssl false
-npm config set proxy http://proxy.example.com:8080
-npm config set https-proxy http://proxy.example.com:8080
-npm config set registry http://registry.example.com:8080
