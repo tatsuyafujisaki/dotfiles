1551d8b1128037b18c56f07a237c87bc9c879d24
Tatsuya Fujisaki <1838962+tatsuyafujisaki@users.noreply.github.com>
2020-02-08 16:23:43 +0900

    Update .eslintrc.js

--- eslint/.eslintrc.js
+++ eslint/.eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-    'env': {
-        'browser': true
-        'node': true,
-        'es2020': true,
-    },
-    'extends': [
-      'airbnb-base',
-    ],
-    'globals': {
-      'Atomics': 'readonly',
-      'SharedArrayBuffer': 'readonly',
-    },
-    'parserOptions': {
-        'ecmaVersion': 2020,
-        'sourceType': 'module',
-        'ecmaFeatures': {
-            impliedStrict: true
-        }
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      impliedStrict: true
     }
+  }
 };
